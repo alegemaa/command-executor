7040b175d52e19f51f23a3cd3c709412b71de7bc
alfonsserra <alfonsoserra@gmail.com>
2019-02-21 16:03:31 +0100

    Implement OS call

---
+++ src/main/java/com/systelab/command/CommandExecutorApp.java
@@ -0,0 +1,59 @@
+package com.systelab.command;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+public class CommandExecutorApp {
+
+    private static class StreamGobbler implements Runnable {
+        private InputStream inputStream;
+        private Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
+        }
+    }
+
+    public CommandExecutorApp() {
+        System.out.println("Here I am!!");
+    }
+
+    private boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().startsWith("windows");
+    }
+
+    public void runCommand() throws Exception {
+        ProcessBuilder builder = new ProcessBuilder();
+        if (isWindows()) {
+            builder.command("cmd.exe", "/c", "dir");
+        } else {
+            builder.command("sh", "-c", "ls");
+        }
+        builder.directory(new File(System.getProperty("user.home")));
+        Process process = builder.start();
+        StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+        Executors.newSingleThreadExecutor().submit(streamGobbler);
+        int exitCode = process.waitFor();
+        assert exitCode == 0;
+    }
+
+    public static void main(String[] args) {
+        CommandExecutorApp app = new CommandExecutorApp();
+        try {
+            app.runCommand();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
